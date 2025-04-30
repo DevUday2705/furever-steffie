5bb064d067b110be7faad7208de4908b70d00e2b
udayk-cognavi <uday.k@cognavi.com>
2025-04-30 17:52:28 +0530

    Updated categories

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -10,8 +10,8 @@ const Home = () => {
   return (
     <div>
       <MobileHeroCarousel />
-      {/* <Categories /> */}
-      <KurtaBanner />
+      <Categories />
+      {/* <KurtaBanner /> */}
       <WatchAndBuy />
       <TrendingProducts />
       <Footer />
