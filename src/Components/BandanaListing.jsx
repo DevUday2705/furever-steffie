ae80091f8af30ef01bb0c8554cad217b05155f61
udayk-cognavi <uday.k@cognavi.com>
2025-05-12 15:19:18 +0530

    Bandana Update

--- src/Components/BandanaListing.jsx
+++ src/Components/BandanaListing.jsx
@@ -1,20 +1,34 @@
-import React from "react";
-import Lottie from "react-lottie";
-import comingSoon from "../../public/animation/coming-soon.json";
-const defaultOptions = {
-  loop: true,
-  autoplay: true,
-  animationData: comingSoon,
-  rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice",
-  },
-};
+import { useState, useEffect } from "react";
+
+import { kurtas } from "../constants/constant";
+
+import ProductListing from "./ProductListing";
 
 const BandanaListing = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  // 1) Base list
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 500);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-800"></div>
+      </div>
+    );
+  }
+
   return (
-    <div className="min-h-screen w-full flex items-start justify-center ">
-      <Lottie options={defaultOptions} height={200} />
-    </div>
+    <ProductListing
+      title="All Kurtas"
+      subtitle="Explore our exclusive range of handcrafted kurtas for pets!"
+      category="kurta"
+      bannerImage="https://res.cloudinary.com/di6unrpjw/image/upload/v1746007679/banner-min_pbtnwp.webp"
+      products={kurtas}
+      bannerTitle="Elegant Kurtas for Every Pet Personality"
+    />
   );
 };
 
