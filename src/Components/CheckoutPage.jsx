32254ce123b50f01aec13c8fa723f1334cf7a22c
udayk-cognavi <uday.k@cognavi.com>
2025-08-22 01:18:20 +0530

    Asked customer to wait..

--- src/Components/CheckoutPage.jsx
+++ src/Components/CheckoutPage.jsx
@@ -346,7 +346,7 @@ const CheckoutPage = () => {
     <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
       <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-indigo-600"></div>
       <p className="ml-4 text-lg font-medium text-indigo-600">
-        Processing your order...
+        Processing your order...Please wait. Do not refresh or close this page.
       </p>
     </div>
   ) : (
