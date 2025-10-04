e8d847e73d2faaefe034a4630bb97ef29918c6ec
udayk-cognavi <uday.k@cognavi.com>
2025-10-05 02:36:06 +0530

    fix: Update nodemailer authentication to use hardcoded credentials in notification handlers

--- api/send-shipped-notification.js
+++ api/send-shipped-notification.js
@@ -28,8 +28,8 @@ export default async function handler(req, res) {
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASSWORD
+                user: 'fureversteffie@gmail.com',
+                pass: 'htyq oijh ugoi echv' // App password
             }
         });
 
