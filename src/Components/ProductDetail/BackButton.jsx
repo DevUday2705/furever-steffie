03a9d1c02adfb5ec645de936d8953f936d0e4b18
devuday2705 <devuday2705@gmail.com>
2025-10-09 02:35:24 +0530

    Updated

--- src/Components/ProductDetail/BackButton.jsx
+++ src/Components/ProductDetail/BackButton.jsx
@@ -4,7 +4,7 @@ import { ChevronLeft } from "lucide-react";
 const BackButton = ({ onClick }) => {
   return (
     <div className="bg-white shadow-sm sticky top-0 z-10">
-      <div className="container mx-auto px-3 py-3">
+      <div className="container mx-auto px-3 py-2">
         <button
           onClick={onClick}
           className="inline-flex items-center text-gray-600"
